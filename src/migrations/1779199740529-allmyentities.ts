d898284b091b867491b155e8c91d4a617e51b82f
Ssanti <santiagorobles107k@gmail.com>
2026-05-19 11:39:46 -0300

    feat(migrations): add user table creation in migration script

--- src/migrations/1779199740529-allmyentities.ts
+++ src/migrations/1779199740529-allmyentities.ts
@@ -4,6 +4,16 @@ export class Allmyentities1779199740529 implements MigrationInterface {
     name = 'Allmyentities1779199740529'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS "user" (
+            "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+            "name" text NOT NULL,
+            "password" text NOT NULL,
+            "DNI" integer,
+            "roles" text[] NOT NULL DEFAULT '{docente}',
+            "isActive" boolean NOT NULL DEFAULT true,
+            CONSTRAINT "PK_8e8d9b8e5b2bbbe236bd2abb5a9" PRIMARY KEY ("id"),
+            CONSTRAINT "UQ_8e8d9b8e5b2bbbe236bd2abb5a9" UNIQUE ("name")
+        )`);
         await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "roles" SET DEFAULT '{docente}'`);
         await queryRunner.query(`ALTER TYPE "public"."aula_state_enum" RENAME TO "aula_state_enum_old"`);
         await queryRunner.query(`CREATE TYPE "public"."aula_state_enum" AS ENUM('available', 'maintenance', 'busy')`);
