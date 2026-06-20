6c34a25d1d07d2eb9834c014404d94bccc72707f
Santiago107R <santiagorobles107k@gmail.com>
2026-06-20 19:55:59 -0300

    refactor(errors): reorder HttpException handling in handleError function

--- src/common/utils/handle-errors.ts
+++ src/common/utils/handle-errors.ts
@@ -3,16 +3,16 @@ import { BadRequestException, ForbiddenException, HttpException, InternalServerE
 const logger = new Logger
 
 export function handleError(error: any): never {
+    if (error instanceof HttpException) {
+        throw error;
+    }
+
     if (error?.code === '23505') {
         throw new BadRequestException(error.detail);
     }
 
     if (error?.code === '23502') {
         throw new BadRequestException(`Missing required value for column: ${error.column}`);
-    }
-
-    if (error instanceof HttpException) {
-        throw error;
     }
 
     logger.error(error);
